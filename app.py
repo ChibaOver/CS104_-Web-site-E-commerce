5a7be6afe09d62d3abe8c2a337910e517ae50263
ChibaOver <153432095+ChibaOver@users.noreply.github.com>
2026-05-24 10:33:48 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -3,23 +3,18 @@ import sqlite3
 
 app = Flask(__name__)
 
-# เชื่อมต่อฐานข้อมูล
 def get_db_connection():
     conn = sqlite3.connect('web.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-# หน้าแรก
 @app.route('/')
 def home():
     conn = get_db_connection()
-
-    # ดึงข้อมูลสินค้า
     products = conn.execute('SELECT * FROM products').fetchall()
-
     conn.close()
 
     return render_template('index.html', products=products)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
