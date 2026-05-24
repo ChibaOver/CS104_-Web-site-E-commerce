4a8ed66395ab612a651e36f2a1ee94830e060f65
ChibaOver <153432095+ChibaOver@users.noreply.github.com>
2026-05-24 12:12:22 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sqlite3
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = sqlite3.connect('database.sql.sqbpro')
+    conn = sqlite3.connect('web.db')
     conn.row_factory = sqlite3.Row
     return conn
 
